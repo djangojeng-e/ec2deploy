bee9bcb8c2d30de3f414deacad100d5aeedf8788
djangojeng-e <headfat1218@gmail.com>
2020-07-31 19:00:10 +0900

    secrets and db file paths set

--- deploying_site/deploying_site/settings.py
+++ deploying_site/deploying_site/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'deploying_site.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(ROOT_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(SECRET_DIR, 'db.sqlite3'),
     }
 }
 
